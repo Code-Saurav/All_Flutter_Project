6b97bad891855b1cc5ee7be46b1cc71e7a38e289
Saurav Sah <saurav.sah@mavs.uta.edu>
2021-08-04 14:37:01 -0500

    final commit

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -12,7 +12,11 @@ class BallPage extends StatelessWidget {
       backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.blue.shade900,
-        title: Text('Ask Me Anything'),
+        centerTitle: true,
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(fontFamily: 'PaletteMosaic'),
+        ),
       ),
       body: Ball(),
     );
@@ -29,13 +33,32 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(
-        onPressed: () {
-          setState(() {
-            ballNumber = Random().nextInt(5) + 1;
-          });
-        },
-        child: Image.asset('images/ball$ballNumber.png'),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 150, 0, 0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'You May Start the game',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontFamily: 'PaletteMosaic'),
+            ),
+          ),
+        ],
       ),
     );
   }
