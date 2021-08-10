810b73f48638b63df09f5dc6a8e9ba3eb11c970e
Saurav Sah <saurav.sah@mavs.uta.edu>
2021-08-10 18:23:57 -0500

    class used

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,11 +26,25 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = []; // initialize empty lists
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Widget> scoreKeeper = [];
+  // initialize empty lists
+//   List<String> questions = [
+//     'You can lead a cow down stairs but not up stairs.',
+//     'Approximately one quarter of human bones are in the feet.',
+//     'A slug\'s blood is green.',
+//   ];
+//
+//   int questionNumber = 0;
+//
+//   List<bool> answers = [false, true, true];
+// //new question object
+//   Question q1 = Question( q: 'You can lead a cow down stairs but not up stairs.', a: false);
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
   ];
 
   int questionNumber = 0;
@@ -46,7 +61,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,12 +88,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  bool correctAnswer =
+                      questionBank[questionNumber].questionAnswer;
+                  if (correctAnswer == true) {
+                    print('User got it right!');
+                  } else {
+                    print('User got it wrong!');
+                  }
                   questionNumber++;
                 });
                 //The user picked true.
