6d390423ef3461d67309205155d627b52826b9e0
Saurav Sah <saurav.sah@mavs.uta.edu>
2021-08-14 13:14:24 -0500

    step 15

--- destini-challenge-starting/lib/main.dart
+++ destini-challenge-starting/lib/main.dart
@@ -43,7 +43,6 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     storyBrain.getStory(),
                     textAlign: TextAlign.center,
                     style: TextStyle(
@@ -66,8 +65,7 @@ class _StoryPageState extends State<StoryPage> {
                         MaterialStateProperty.all<Color>(Colors.red),
                   ),
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -93,8 +91,7 @@ class _StoryPageState extends State<StoryPage> {
                         MaterialStateProperty.all<Color>(Colors.blue),
                   ),
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
