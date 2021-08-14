074a80077056a7aca4da7a60d1ca6fd41cf22d18
Saurav Sah <saurav.sah@mavs.uta.edu>
2021-08-14 13:53:26 -0500

    step 23

--- destini-challenge-starting/lib/main.dart
+++ destini-challenge-starting/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -56,7 +54,8 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+
+                    storyBrain.nextStory(1);
                   },
                   style: ButtonStyle(
                     foregroundColor:
@@ -81,8 +80,8 @@ class _StoryPageState extends State<StoryPage> {
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: TextButton(
                   onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    //Choice 2 made by user
+                    storyBrain.nextStory(2);
                   },
                   style: ButtonStyle(
                     foregroundColor:
